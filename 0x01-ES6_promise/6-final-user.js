c9e8dbad5277a7983de16d968459852085c21ae3
Winfred <winfrednginakilonzo@gmail.com>
2023-11-29 15:50:02 +0300

    Fix bug

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,18 +4,14 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const profilePromise = uploadPhoto(fileName);
+
   return Promise.allSettled([userPromise, profilePromise])
-    .then(([user, profile]) => {
-      const resultsArray = [
-        {
-          status: user.status,
-          value: user.status === 'fulfilled' ? user.value : user.reason,
-        },
-        {
-          status: profile.status,
-          value: profile.status === 'fulfilled' ? profile.value : profile.reason,
-        },
-      ];
+    .then((results) => {
+      const resultsArray = results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
+
       return resultsArray;
     });
 }
