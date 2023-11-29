9c86ebede7f400077fa138292e9e845a65248d17
Winfred <winfrednginakilonzo@gmail.com>
2023-11-29 13:01:58 +0300

    Return error object

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => new Error())
+    .catch((error) => error)
     .finally(() => {
       console.log('Got a response from the API');
     });
