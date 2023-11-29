6f270926d813e34d537a9e54a9f124d7cf4a8bc9
Winfred <winfrednginakilonzo@gmail.com>
2023-11-29 12:51:44 +0300

    Removed error variable

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,8 +4,8 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return ({ status: 200, body: 'Success' });
     })
-    .catch((error) => {
-      console.log('Got a response from the API', error);
+    .catch(() => {
+      console.log('Got a response from the API');
       return new Error();
     });
 }
