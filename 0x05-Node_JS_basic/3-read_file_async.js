b14d72c0cba17c50c187e6d74eafb00475a11ae9
Winfred <winfrednginakilonzo@gmail.com>
2024-01-30 11:59:49 +0300

    Read file asynchronously

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,45 @@
-const fs = require('fs');
-const { resolve } = require('path');
+const fs = require('fs').promises;
 
-function countStudents (path) {
+function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8')
       .then((data) => {
         const rows = data.trim().split('\n');
-        const fieldsCounts = {};
-        for (let i = 1; i < rows.length; i++) {
+        const fieldCounts = {};
+
+        for (let i = 1; i < rows.length; i += 1) {
           const [firstname, lastname, age, field] = rows[i].split(',');
+
           if (firstname && lastname && age && field) {
             if (fieldCounts[field]) {
               // If the field already exists in fieldCounts, increment the count by 1
-              fieldCounts[field] = fieldCounts[field] + 1;
+              fieldCounts[field] += 1;
             } else {
               // If the field doesn't exist in fieldCounts, initialize the count to 1
               fieldCounts[field] = 1;
             }
           }
         }
+
+        console.log(`Number of students: ${rows.length - 1}`); // subtracting 1 for the header
+
         for (const field in fieldCounts) {
-          const count = fieldCounts[field];
-          const studentsList = rows
-            .slice(1) // Skipping the header
-            .fielter((row) => row.split(',')[3] === field)
-            .map((row) = row.split(',')[0]);
-          console.log(`Number of students in ${field}: ${count}. List: ${studentsList.join(', ')}`);
+          if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
+            const count = fieldCounts[field];
+            const studentsList = rows
+              .slice(1) // Skipping the header
+              .filter((row) => row.split(',')[3] === field)
+              .map((row) => row.split(',')[0]);
+            console.log(`Number of students in ${field}: ${count}. List: ${studentsList.join(', ')}`);
+          }
         }
+
         resolve();
       })
-      .catch((error) => {
+      .catch(() => {
         reject(new Error('Cannot load the database'));
       });
   });
 }
+
 module.exports = countStudents;
